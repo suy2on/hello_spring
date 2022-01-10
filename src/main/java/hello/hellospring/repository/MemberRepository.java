6b4755ac7b7cbf01fd73279e15d4717fdb576260
suy2on <tnwjdrms@naver.com>
2022-01-10 14:01:55 +0900

    # feat : 회원정보다루는 인터페이스정의

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.Optional;
+import java.util.List;
+
+public interface MemberRepository {
+    Member save(Member member); // 회원저장
+    Optional<Member> findById(Long id); // 회원조회
+    Optional<Member> findByName(String name);
+    List<Member> findAll(); // 모든회원조회
+}
